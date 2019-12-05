40c218ab84c33d11aa174ee429838814a1a2b98b
mizzick <ds.kolyshev@gmail.com>
2019-12-05 22:47:36 +0700

    #84 review

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import './lib/style-property-matcher';
 import './lib/extended-css-selector';
 import './lib/extended-css-parser';
 import './lib/sizzle.patched';
-import {ExtendedCss} from './lib/extended-css';
+import ExtendedCss from './lib/extended-css';
 
 export default ExtendedCss;
